15bf3673d5cba0f7e3a2811bc055dd9e65b22e30
Zac Colley <hi@zaccolley.com>
2013-04-08 22:47:54 +0100

    Getting data from file and finding the domains

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,9 @@
 
 // Function for fallback from JSONP
 wikiData = function(json){
-	var output = "";
-	try{ output = processWikiData(json).join(' | '); }
-	catch(error){ output = '<p>Cannot get data from Wikipedia. <span class="error">Error: (' + error + ')</span></p>'; }
-	$('body').html('<p>' + output + '</p>');
+	try{ var output = processWikiData(json); }
+	catch(error){ var output = '<p>Cannot get data from Wikipedia. <span class="error">Error: (' + error + ')</span></p>'; }
+	processWords(output);
 };
 
 // Process the JSON and get back an array of domain names
@@ -16,7 +15,7 @@ function processWikiData(json){
 	var domains = [];
 	while(regEx.test(text)){ // Loops while there is an index of the regex
 		domain = regEx.exec(text)[0]; // Grabs the string that it matches
-		domains.push(domain.substring(2, domain.length - 2)); // Adds the domain prettified to the array
+		domains.push(domain.substring(3, domain.length - 2)); // Adds the domain prettified to the array
 		text = text.replace(regEx, ''); // Removes it from the text we're searching
 	}
 	return domains;
@@ -27,4 +26,41 @@ function loadWikiData(){
 	var title = 'List_of_Internet_top-level_domains'; // Title of the Wikipedia page
 	var url = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&callback=wikiData&titles=' + title;
 	$.ajax({ url: url, dataType: 'script', async: false });
+}
+
+// Stick in an array of words and it'll reverse them all.
+function reverseWords(input){
+	var output = [];
+	for(var i = 0; i < input.length; i ++){
+		output[i] = input[i].split('').reverse().join('');
+	}
+	return output;
+}
+
+//
+function processWords(domains){
+	var url = './mwords/113809of.fic';
+	$.ajax({ url: url, dataType: 'text', async: false })
+	.done(function(data){
+		words = data.split('\n');
+		var reversedDomains = reverseWords(domains).sort();
+		var reversedWords = reverseWords(words).sort();
+		console.log(reversedDomains);
+		console.log(reversedWords);
+		matched = [];
+		for(var i = 0; i < reversedWords.length; i++){
+			for(var j = 0; j < reversedDomains.length; j++){
+				var reversedDomain = reversedDomains[j].toString();
+				var trimmedReversedWord = reversedWords[i].substring(0, reversedDomains[j].length + 1).toString().trim();
+				var restOfWord = reversedWords[i].substring(reversedDomains[j].length + 1);
+				if(reversedDomain == trimmedReversedWord){
+					if(restOfWord != ''){
+						matched.push(reversedDomain +"."+ restOfWord);
+					}
+				}
+			}
+		}
+		console.log(matched);
+		$('body').html('<p>' + reverseWords(matched).join(' | ') + '</p>');
+	});
 }
