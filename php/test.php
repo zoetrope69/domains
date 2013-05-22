4bba62419bdceb63a597f283bdc7135369e9cf03
Zac Colley <hi@zaccolley.com>
2013-05-22 18:01:34 +0100

    Added wordnik PHP stuff

---
+++ php/test.php
@@ -0,0 +1,9 @@
+<?php
+	require('connect.php');
+	$wordApi = new WordApi($client);
+	$word = $_POST['word'];
+	$definitions = $wordApi->getDefinitions($word, $partOfSpeech=null, $sourceDictionaries=null, $limit=1);
+	$definition = $definitions[0]->text;
+	$source = $definitions[0]->sourceDictionary;
+	print '<span title=\'Source: '.$source.'\'>' .$definition. '</span>';
+?>
