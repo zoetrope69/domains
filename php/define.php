82bbb20f8c288021d8f50de9f7feda34eefb444b
Zac Colley <hi@zaccolley.com>
2013-07-27 14:09:22 +0100

    Check for post request and strip HTML tags on define.php

--- php/define.php
+++ php/define.php
@@ -2,15 +2,28 @@
 	require('connect.php');
 
 	$wordApi = new WordApi($client);
-	$word = $_POST['word'];
 
-	if($definitions = $wordApi->getDefinitions($word, $partOfSpeech=null, $sourceDictionaries=null, $limit=1)){
-		$definition = $definitions[0]->text;
-		$source = $definitions[0]->sourceDictionary;
-		$output = '<span title="Source: '.$source.'">' .$definition. '</span>';
+	// if it's a post request
+	if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+		// get the word and convert any html
+		$word = htmlentities($_POST['word']);
+
+		// if getting a definition brings back a results
+		if($definitions = $wordApi->getDefinitions($word, $partOfSpeech=null, $sourceDictionaries=null, $limit=1)){
+			
+			$definition = $definitions[0]->text;
+			$source = $definitions[0]->sourceDictionary;
+			print '<span title="Source: '.$source.'">' .$definition. '</span>';
+
+		// else no definition, get fall back
+		}else{
+			print 'Erm, couldnt find a definition for "'.$word.'"... Try <a href="http://dictionary.reference.com/browse/'.$word.'">looking it up</a> here?';
+		}
+	
+	// not a post request? print a snarky comment
 	}else{
-		$output = 'Erm, couldnt find a definition for "'.$word.'"... Try <a href="http://dictionary.reference.com/browse/'.$word.'">looking it up</a> here?';
+		print 'What are you doing here? Use the app!';
 	}
 
-	print $output;
 ?>
