bd51c630064cf2a6179b27e93c649d15b021ff64
Zac Colley <hi@zaccolley.com>
2014-02-22 12:55:49 +0000

    Added jade file detection the the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function(grunt) {
             },
 
             html: {
-                files: ['*.html'],
+                files: ['*.html', '**/*.jade'],
                 options: {
                     spawn: false
                 }
