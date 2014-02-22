65d1c45e8bfceb0f7d3c7216df70a8c995098a7b
Zac Colley <hi@zaccolley.com>
2014-02-22 11:03:53 +0000

    Removed loads of shit because we're starting again

--- php/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', '1');
-
-	require('wordnik/Swagger.php');
-	$myAPIKey = 'fabba71775ec0ac0e200f05d1910b2778b90d5f5fc37e5569';
-	$client = new APIClient($myAPIKey, 'http://api.wordnik.com/v4');
-?>
