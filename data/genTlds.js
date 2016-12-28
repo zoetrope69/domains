4df01f4a6ba6f4698b52900d2e0a171ec6930efd
Zac Colley <hi@zaccolley.com>
2016-12-28 15:20:26 +0000

    Improved data scripts

--- data/genTlds.js
+++ data/genTlds.js
@@ -1,43 +1,31 @@
 // get the tlds
 
-var http = require('http'),	
-  	fs = require('fs');
+var http = require('http');
+var fs = require('fs');
 
-http.get("http://data.iana.org/TLD/tlds-alpha-by-domain.txt", function(res) {
-	console.log("Got response: " + res.statusCode + "\n");
+http.get('http://data.iana.org/TLD/tlds-alpha-by-domain.txt', function(res) {
+	console.log('Got response:', res.statusCode);
 
-	var data = new Buffer("");
+	var data = new Buffer('');
 
-	res.on('data', function(chunk){
+	res.on('data', function(chunk) {
 		data = Buffer.concat([data, chunk]);
 	});
 
-	res.on('end', function (){
-		var domains = data.toString('utf8').split("\n");
+	res.on('end', function () {
+		var tlds = data.toString('utf8').split('\n');
 
-		var tempDomains = [];
-		for(var d =  0; d < domains.length; d++){
-			var domain = domains[d];
+    tlds = tlds.filter(tld => tld.match(/^[A-z]+$/g)).map(tld => tld.toLowerCase());
 
-			// no characters but alpha
-			if(domain.match(/^[A-z]+$/g)){
-				tempDomains.push(domain.toLowerCase());
+		fs.writeFile(__dirname + '/tlds.json', JSON.stringify(tlds, null, 2), function(error) {
+			if (error) {
+				return console.log('Error:', error);
 			}
-		}
 
-		domains = tempDomains;
-
-		console.log(domains);
-
-		fs.writeFile(__dirname+'/tlds.json', JSON.stringify(domains, null, 2), function(e){
-			if(e){
-				console.log("Error: ", e);
-			}else{
-				console.log("All golden. File saved.");
-			}
+      console.log(tlds.length, 'TLDs saved to /tlds.json');
 		});
 	});
 
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
+}).on('error', function(error) {
+  console.log('Error:', error.message);
 });
